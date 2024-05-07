ef51d8fe8f3f15120b99d8d1a2f58b27521860f3
Efrat7248 <efrat7248@gmail.com>
2024-05-07 15:03:40 +0300

    confilct main and branch2

--- file1.js
+++ file1.js
@@ -2,6 +2,5 @@
 const func=()=>{
     alert("this is conflict with main and branch2 ")
     alert(" conflict main ")
-    alert(" conflict branch2 ")
 
 return "this is file 1 create in branch main" }
